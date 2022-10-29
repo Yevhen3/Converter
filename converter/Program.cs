90accd86e3a45278ef074605c99e96e781c2eefa
Yevhen3 <114805048+Yevhen3@users.noreply.github.com>
2022-10-29 13:26:06 +0300

    Update Program.cs

--- converter/Program.cs
+++ converter/Program.cs
@@ -1,36 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Enter sum of money you want to convert");
-int sum = int.Parse(Console.ReadLine());
-Converte converter = new Converte(36.82, 35.86);
-Console.WriteLine(converter.ConvertCurrency(sum));
-class Converte
+try
 {
-    private double dollarRate;
-    private double euroRate;
-    public Converte(double dollarRate, double euroRate)
+    Console.WriteLine("Enter sum of money you want to convert");
+    decimal sum = decimal.Parse(Console.ReadLine());
+    Converter converter = new Converter(37.1m, 36.5m);
+    Console.WriteLine(converter.ConvertUAHtoEURO(sum));
+}
+catch
+{
+    Console.WriteLine("Your input is incorrect");
+}
+
+class Converter
+{
+    private decimal dollarRate;
+    private decimal euroRate;
+    public Converter(decimal dollarRate, decimal euroRate)
     {
         this.dollarRate = dollarRate;
         this.euroRate = euroRate;
+ 
     }
-    public double ConvertCurrency(double SumOfMoney)
+    public decimal ConvertUAHtoUSD(decimal sumOfmoney)
     {
-        double result;
-        Console.WriteLine("Enter: \n1 uah->usd \n2 usd->uah \n3 uah->euro \n4 euro->uah");
-        int choice = int.Parse(Console.ReadLine());
-        Console.Write("Result is ");
-        switch (choice)
-        {
-            case 1:
-                return SumOfMoney / dollarRate;
-            case 2:
-                return SumOfMoney / euroRate;
-            case 3:
-                return SumOfMoney * dollarRate;
-            case 4:
-                return SumOfMoney * euroRate;
-
-        }
-        return 0;
+        return sumOfmoney/dollarRate;
     }
+    public decimal ConvertUSDtoUAH(decimal sumOfmoney)
+    {
+        return sumOfmoney * euroRate;
+    }
+    public decimal ConvertUAHtoEURO(decimal sumOfmoney)
+    {
+      
+        return  sumOfmoney / dollarRate;
+    }
+    public decimal ConvertEUROtoUAH(decimal sumOfmoney)
+    {
+        return sumOfmoney * euroRate;
+    }
+    
 }
